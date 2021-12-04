e8546d450e13017562d1b48d1827b985eaa9dd4a
LinHeLurking <LinHe.Lurking@gmail.com>
2021-12-04 13:48:12 +0800

    Dynamically eval over tree

--- Symbolic/Expression.h
+++ Symbolic/Expression.h
@@ -9,7 +9,7 @@
 
 #include "Number.h"
 
-namespace Spp::Expression {
+namespace Spp::Expr {
 
     // L1: +, -
     // L2: *, /
@@ -17,18 +17,19 @@ namespace Spp::Expression {
     // LN_OP: Numeric
     constexpr int L1_OP = 1, L2_OP = 2, L3_OP = 3, LN_OP = 10000;
 
-    template<typename ValueT, std::enable_if_t<Numeric::is_numeric_v < ValueT>, bool> = true>
+    enum ExprNodeType {
+        Num, Add, Sub, Mul, Div, Cast
+    };
 
+    template<typename ValueT>
     class NodeBase {
     public:
-        // For numeric nodes, just returning is enough.
-        // For op nodes, evaluation over the sub-tree.
-        // Values will be cached.
-        virtual ValueT val() = 0;
+        explicit NodeBase(ExprNodeType type, int pred) : pred_(pred), type_(type) {}
+
+        explicit NodeBase() = delete;
 
         int pred_;
-
-        explicit NodeBase(int pred) : pred_(pred) {}
+        ExprNodeType type_;
 
         [[nodiscard]] virtual std::string expr_to_string() const = 0;
     };
@@ -44,55 +45,35 @@ namespace Spp::Expression {
     template<typename ValueT>
     class NumericNode : public NodeBase<ValueT> {
     public:
-        ValueT val() override {
-            return val_;
-        }
-
-        explicit NumericNode(ValueT val) : val_(val), NodeBase<ValueT>(LN_OP) {}
+        explicit NumericNode(ValueT val) : val_(val), NodeBase<ValueT>(ExprNodeType::Num, LN_OP) {}
 
         [[nodiscard]] std::string expr_to_string() const override {
             return to_string(val_);
         }
 
-    protected:
         ValueT val_;
     };
 
     template<typename ValueT>
     class OperatorNode : public NodeBase<ValueT> {
     public:
-        inline ValueT val() override {
-            if (this->cache_initialized_) {
-                return this->cached_val_;
-            } else {
-                std::vector<ValueT> child_val;
-                for (auto child: children_) {
-                    child_val.push_back(child->val());
-                }
-                this->cache_initialized_ = true;
-                return this->cached_val_ = op(child_val);
-            }
-        }
-
         template<class StringT>
-        explicit OperatorNode(int pred, StringT name) :NodeBase<ValueT>(pred), name_(name) {}
+        explicit OperatorNode(ExprNodeType type, int pred, StringT name) :NodeBase<ValueT>(type, pred), name_(name) {}
 
         virtual ValueT op(const std::vector<ValueT> &v) = 0;
 
         const std::string name_;
 
         std::vector<SharedNode<ValueT>> children_;
-
-    protected:
-        bool cache_initialized_ = false;
-        ValueT cached_val_ = static_cast<ValueT>(0);
     };
 
     template<typename ValueT>
-    class BinaryOpNode : public OperatorNode<ValueT> {
+    class ArithmeticNode : public OperatorNode<ValueT> {
     public:
         template<typename T, typename U>
-        explicit BinaryOpNode(T a, U b, int pred, std::string name) : OperatorNode<ValueT>(pred, name) {
+        explicit
+        ArithmeticNode(T a, U b, ExprNodeType type, int pred, std::string name)
+                : OperatorNode<ValueT>(type, pred, name) {
             this->children_.push_back(SharedNode<ValueT>(new T(a)));
             this->children_.push_back(SharedNode<ValueT>(new U(b)));
         }
@@ -107,10 +88,11 @@ namespace Spp::Expression {
     };
 
     template<typename ValueT>
-    class AddNode : public BinaryOpNode<ValueT> {
+    class AddNode : public ArithmeticNode<ValueT> {
     public:
         template<typename T, typename U>
-        explicit AddNode(T a, U b):BinaryOpNode<ValueT>(a, b, L1_OP, "+") {}
+        explicit AddNode(T a, U b)
+                :ArithmeticNode<ValueT>(a, b, ExprNodeType::Add, L1_OP, "+") {}
 
         ValueT op(const std::vector<ValueT> &v) override {
             return v[0] + v[1];
@@ -118,10 +100,11 @@ namespace Spp::Expression {
     };
 
     template<typename ValueT>
-    class SubNode : public BinaryOpNode<ValueT> {
+    class SubNode : public ArithmeticNode<ValueT> {
     public:
         template<typename T, typename U>
-        explicit SubNode(T a, U b):BinaryOpNode<ValueT>(a, b, L1_OP, "-") {}
+        explicit SubNode(T a, U b)
+                :ArithmeticNode<ValueT>(a, b, ExprNodeType::Sub, L1_OP, "-") {}
 
         ValueT op(const std::vector<ValueT> &v) override {
             return v[0] - v[1];
@@ -129,10 +112,11 @@ namespace Spp::Expression {
     };
 
     template<typename ValueT>
-    class MulNode : public BinaryOpNode<ValueT> {
+    class MulNode : public ArithmeticNode<ValueT> {
     public:
         template<typename T, typename U>
-        explicit MulNode(T a, U b):BinaryOpNode<ValueT>(a, b, L2_OP, "*") {}
+        explicit MulNode(T a, U b)
+                :ArithmeticNode<ValueT>(a, b, ExprNodeType::Mul, L2_OP, "*") {}
 
         ValueT op(const std::vector<ValueT> &v) override {
             return v[0] * v[1];
@@ -140,59 +124,95 @@ namespace Spp::Expression {
     };
 
     template<typename ValueT>
-    class DivNode : public BinaryOpNode<ValueT> {
+    class DivNode : public ArithmeticNode<ValueT> {
     public:
         template<typename T, typename U>
-        explicit DivNode(T a, U b):BinaryOpNode<ValueT>(a, b, L2_OP, "/") {}
+        explicit DivNode(T a, U b)
+                :ArithmeticNode<ValueT>(a, b, ExprNodeType::Div, L2_OP, "/") {}
 
         ValueT op(const std::vector<ValueT> &v) override {
             return v[0] / v[1];
         }
     };
 
-    template<typename ValueT, template<class ...> class T,
-            template<class ...> class U>
+    template<typename ValueT,
+            template<class> class T,
+            template<class> class U,
+            std::enable_if_t<
+                    (std::is_base_of_v<NodeBase<ValueT>, T<ValueT>> ||
+                     std::is_base_of_v<NodeBase<ValueT>, U<ValueT>>), bool> = true>
     AddNode<ValueT> operator+(T<ValueT> a, U<ValueT> b) {
         return AddNode<ValueT>(a, b);
     }
 
-    template<typename ValueT, template<class ...> class T,
-            template<class ...> class U>
+    template<typename ValueT,
+            template<class> class T,
+            template<class> class U,
+            std::enable_if_t<
+                    (std::is_base_of_v<NodeBase<ValueT>, T<ValueT>> ||
+                     std::is_base_of_v<NodeBase<ValueT>, U<ValueT>>), bool> = true>
     SubNode<ValueT> operator-(T<ValueT> a, U<ValueT> b) {
         return SubNode<ValueT>(a, b);
     }
 
-    template<typename ValueT, template<class ...> class T,
-            template<class ...> class U>
+    template<typename ValueT,
+            template<class> class T,
+            template<class> class U,
+            std::enable_if_t<
+                    (std::is_base_of_v<NodeBase<ValueT>, T<ValueT>> ||
+                     std::is_base_of_v<NodeBase<ValueT>, U<ValueT>>), bool> = true>
     MulNode<ValueT> operator*(T<ValueT> a, U<ValueT> b) {
         return MulNode<ValueT>(a, b);
     }
 
-    template<typename ValueT, template<class ...> class T,
-            template<class ...> class U>
+    template<typename ValueT,
+            template<class> class T,
+            template<class> class U,
+            std::enable_if_t<
+                    (std::is_base_of_v<NodeBase<ValueT>, T<ValueT>> ||
+                     std::is_base_of_v<NodeBase<ValueT>, U<ValueT>>), bool> = true>
     DivNode<ValueT> operator/(T<ValueT> a, U<ValueT> b) {
         return DivNode<ValueT>(a, b);
     }
 
-
-    template<typename ValueT, typename ChildValueT>
-    class CastNode : public NodeBase<ValueT> {
-    public:
-        ValueT val() override {
-            return static_cast<ValueT>(child_->val());
+    namespace Visitor {
+        template<typename ValueT>
+        inline SharedNode<ValueT> eval_visitor(SharedNode<ValueT> nd) {
+            using std::dynamic_pointer_cast;
+            if (nd->type_ == Num) {
+                return nd;
+            } else if (nd->type_ == Add || nd->type_ == Sub || nd->type_ == Mul || nd->type_ == Div) {
+                // Binary arithmetic
+                auto op_nd = dynamic_pointer_cast<ArithmeticNode<ValueT>>(nd);
+                op_nd->children_[0] = eval_visitor(op_nd->children_[0]);
+                op_nd->children_[1] = eval_visitor(op_nd->children_[1]);
+                if (op_nd->children_[0]->type_ == Num && op_nd->children_[1]->type_ == Num) {
+                    ValueT l_val = dynamic_pointer_cast<NumericNode<ValueT>>(op_nd->children_[0])->val_;
+                    ValueT r_val = dynamic_pointer_cast<NumericNode<ValueT>>(op_nd->children_[1])->val_;
+                    switch (op_nd->type_) {
+                        case Add:
+                            return SharedNode<ValueT>(new NumericNode<ValueT>(l_val + r_val));
+                        case Sub:
+                            return SharedNode<ValueT>(new NumericNode<ValueT>(l_val - r_val));
+                        case Mul:
+                            return SharedNode<ValueT>(new NumericNode<ValueT>(l_val * r_val));
+                        case Div:
+                            return SharedNode<ValueT>(new NumericNode<ValueT>(l_val / r_val));
+                        default: // Impossible!
+                            break;
+                    }
+                }
+            }
+            // All unsupported op fall through here
+            return nd;
         }
 
-        [[nodiscard]] std::string expr_to_string() const override {
-            return Numeric::to_string(static_cast<ValueT>(child_->val()));
+        template<typename ValueT, template<class> class NodeT,
+                std::enable_if_t<std::is_base_of_v<NodeBase<ValueT>, NodeT<ValueT>>, bool> = true>
+        inline SharedNode<ValueT> eval_visitor(const NodeT<ValueT> &nd) {
+            return eval_visitor(SharedNode<ValueT>(new NodeT<ValueT>(nd)));
         }
-
-        template<template<class ...> class ChildNodeT>
-        explicit CastNode(ChildNodeT<ChildValueT> child):NodeBase<ValueT>(child.pred_) {
-            child_ = SharedNode<ChildValueT>(new ChildNodeT<ChildValueT>(child));
-        }
-
-        SharedNode<ChildValueT> child_;
-    };
+    }
 }
 
 #endif //SPP_EXPRESSION_H
--- main.cpp
+++ main.cpp
@@ -7,15 +7,16 @@
 
 using namespace std;
 using namespace Spp::Numeric;
-using namespace Spp::Expression;
+using namespace Spp::Expr;
+using namespace Spp::Expr::Visitor;
 
 int main() {
     auto a = NumericNode(Rational(1, 2));
     auto b = NumericNode(Rational(1, 3));
     auto c = NumericNode(Rational(1, 4));
     auto d = NumericNode(Rational(1, 5));
-    auto res = CastNode<double, Rational<int>>((a + b) * c - c / d);
-    cout << res.val() << endl;
-    cout << ((a + b) * c - c / d).expr_to_string() << endl;
+    auto res = (a + b) * c - c / d;
+    cout << res.expr_to_string() << endl;
+    cout << eval_visitor(res)->expr_to_string() << endl;
     return 0;
 }
